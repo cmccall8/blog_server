fd88a729f597855bc3a9dca854252d7b4f066a94
Claire McCall <clairemccall@admins-MacBook-Pro.local>
2019-06-27 09:12:54 -0600

    blog server

---
+++ data.js
@@ -0,0 +1,40 @@
+//the data that can change should go here
+
+var data = {
+  posts: [
+    {
+      title: "first-post",
+      author: "mr. author",
+      category: "comic books",
+      date: new Date(),
+      image:"https://i.imgur.com/HuwV4CW.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp",
+    },
+    {
+      title: "first-post",
+      author: "mr. author",
+      category: "cards",
+      date: new Date(),
+      image: "https://i.imgur.com/HuwV4CW.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp",
+    },
+    {
+      title: "first-post",
+      author: "mr. author",
+      category: "books",
+      date: new Date(),
+      image: "https://i.imgur.com/HuwV4CW.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp",
+    },
+    {
+      title: "first-post",
+      author: "mr. author",
+      category: "keychains",
+      date: new Date(),
+      image: "https://i.imgur.com/HuwV4CW.jpg",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp",
+    },
+  ]
+};
+
+module.exports = data;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require( "express" );
+const cors = require( "cors" );
+
+var server = express();
+var port = 8080;
+
+//middleware
+server.use(cors());
+server.use(express.json());
+
+//data
+var data = require("./data.js")
+
+//GET
+server.get("/posts", function (req,res){
+  var response = {
+    posts: data.posts
+  };
+  res.json( response );
+});
+
+//POST
+server.post("/posts", function (req,res){
+  var new_post = {
+    title: req.body.title,
+    author: req.body.author,
+    date: req.body.date,
+    image: req.body.image,
+    text: req.body.text,
+  };
+  data.posts.unshift( new_post );
+  res.status( 201 );
+  res.send();
+});
+
+server.listen( port, function (){
+  console.log(`listening on ${port}`)
+});
